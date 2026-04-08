c1872db8548ea2f326e6785456488b23cc5e0dc8
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2026-04-08 14:00:25 -0300

    criação de schemas de usuários

--- car_api/routers/users.py
+++ car_api/routers/users.py
@@ -10,14 +10,17 @@ async def list_users():
         'users': [
             {
                 'id': 1,
+                'username': 'pycodebr',
                 'email': 'pycodebr@gmail.com',
             },
             {
                 'id': 2,
+                'username': 'gmail',
                 'email': 'joao@gmail.com',
             },
             {
                 'id': 3,
+                'username': 'mario',
                 'email': 'mario@gmail.com',
             },
         ]
