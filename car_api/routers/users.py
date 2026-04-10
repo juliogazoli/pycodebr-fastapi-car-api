9f0d5be7a12d7dc2965e8a61e20e6fe1e2bdc8db
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2026-04-10 14:13:30 -0300

    criando usuários

--- car_api/routers/users.py
+++ car_api/routers/users.py
@@ -1,8 +1,27 @@
 from fastapi import APIRouter, status
-from car_api.schemas.users import UserListPublicSchema
+from car_api.db import USERS
+from car_api.schemas.users import (
+    UserListPublicSchema,
+    UserPublicSchema,
+    UserSchema,
+)
 
 
 router = APIRouter()
+
+
+@router.post(
+    path='/',
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserPublicSchema,
+)
+async def create_user(user: UserSchema):
+    user_with_id = UserPublicSchema(
+        **user.model_dump(),
+        id=len(USERS) + 1,
+    )
+    USERS.append(user_with_id)
+    return user_with_id
 
 
 @router.get(
