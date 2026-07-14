86b9a9e54306fa63db66070daef817b074158ce9
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2026-07-14 14:09:57 -0300

    modelagem com type hints

--- car_api/models/users.py
+++ car_api/models/users.py
@@ -1,21 +1,21 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from car_api.models import Base
 
 
 class User(Base):
     __tablename__ = 'users'
-    
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    updated_at = Column(
-        DateTime, 
-        server_default=func.now(),
-        onupdate=func.now()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    updated_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(), server_default=func.now(),
     )
-    created_at = Column(
-        DateTime,
+    created_at: Mapped[datetime] = mapped_column(
         server_default=func.now()
     )
